c250e6a272cebe0dec171eb4825e4ec62aa774b9
riccardopestrin <79696750+riccardopestrin@users.noreply.github.com>
2021-04-07 16:10:16 +0200

    New Classes Created
    Resource + starting FaithPath

---
+++ src/main/java/model/FaithPath.java
@@ -0,0 +1,28 @@
+package model;
+
+public class FaithPath {
+    private int advancement;
+
+
+    public moveForward(int steps) {
+        advancement = advancement + steps;
+    }
+
+    public int checkVictoryPoints(int oldPosition) {
+        return newPoints;
+    }
+
+    public boolean checkPopeSpace
+
+    {
+
+
+    }
+
+    public boolean checkVaticanSection
+    {
+
+    }
+
+
+}
