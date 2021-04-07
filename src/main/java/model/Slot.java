2dd1dcb6cc050157605f5b1ddfd8941960d445eb
riccardopestrin <79696750+riccardopestrin@users.noreply.github.com>
2021-04-07 17:10:48 +0200

    Creation of Slot class

---
+++ src/main/java/model/Slot.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Slot
+{
+    private ArrayList<DevCard> slots;
+
+    public Slot()
+    {
+        
+    }
+
+    public void purchaseDevCard(DevCard card, int slot)
+    {
+
+    }
+
+    public boolean checkSpace(DevCard card, int slot)
+    {
+
+    }
+
+    public DevCards[3] getFrontCards()
+    {
+
+    }
+}
