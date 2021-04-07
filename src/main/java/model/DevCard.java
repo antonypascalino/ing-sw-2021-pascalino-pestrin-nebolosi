426395795fae9c3f397eaedac3191ea82ec76178
riccardopestrin <79696750+riccardopestrin@users.noreply.github.com>
2021-04-07 18:01:54 +0200

    Worked on slot class

---
+++ src/main/java/model/DevCard.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.util.ArrayList;
+
+public class DevCard
+{
+    int level;
+    ArrayList<Resource> price = new ArrayList<Resource>();
+
+}
--- src/main/java/model/Slot.java
+++ src/main/java/model/Slot.java
@@ -1,25 +1,50 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Slot
 {
     private ArrayList<DevCard> slots;
 
     public Slot()
     {
+        ArrayList<DevCard> slots = new ArrayList<DevCard>();
         
     }
 
+    //to do
     public void purchaseDevCard(DevCard card, int slot)
     {
+
+
 
     }
 
     public boolean checkSpace(DevCard card, int slot)
     {
+        //checks if the slot is empty or not
+        if(slots.get(slot) == null)
+        {
+            if (card.level == 1)
+                return true;
+            else
+                return false;
+        }
+        else
+        {
+            //if it isn't, then checks card levels
+            DevCard slotCard = slots.get(slot);
+
+            if(card.level == slotCard.level-1)
+                return true;
+            else
+                return false;
+        }
+
 
     }
 
-    public DevCards[3] getFrontCards()
+    public DevCard[3] getFrontCards()
     {
 
     }
