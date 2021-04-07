230fa0c0e1b678c51518f562db590a06309c82a4
riccardopestrin <79696750+riccardopestrin@users.noreply.github.com>
2021-04-07 16:53:01 +0200

    more work on FaithPath

--- src/main/java/model/FaithPath.java
+++ src/main/java/model/FaithPath.java
@@ -2,25 +2,47 @@ package model;
 
 public class FaithPath {
     private int advancement;
+    private int newPoints;
 
-
-    public moveForward(int steps) {
-        advancement = advancement + steps;
+    public FaithPath()
+    {
+        advancement = 0;
+        newPoints = 0;
     }
 
-    public int checkVictoryPoints(int oldPosition) {
+    // updates the advancement in the faith path
+    public void moveForward(int steps)
+    {
+           advancement = advancement + steps;
+
+    }
+
+
+    public int checkVictoryPoints(int oldPosition)
+    {
+        newPoints = newPoints + oldPosition;
         return newPoints;
     }
 
-    public boolean checkPopeSpace
-
+    //A Pope Space is located every 8 steps
+    public boolean checkPopeSpace()
     {
-
-
+        if (advancement % 8 == 0)
+            return true;
+        else
+            return false;
     }
 
-    public boolean checkVaticanSection
+    
+    public boolean checkVaticanSection()
     {
+        if (checkPopeSpace() == true)
+        {
+            if
+        }
+
+
+
 
     }
 
