4d2edd5ab0dc6d5d416cfd4af7fbb5dd8cdefc21
riccardopestrin <79696750+riccardopestrin@users.noreply.github.com>
2021-04-08 15:41:11 +0200

    Completed Slot class

--- src/main/java/model/Slot.java
+++ src/main/java/model/Slot.java
@@ -4,48 +4,102 @@ import java.util.ArrayList;
 
 public class Slot
 {
-    private ArrayList<DevCard> slots;
+    private ArrayList<DevCard[]> slots;
 
     public Slot()
     {
-        ArrayList<DevCard> slots = new ArrayList<DevCard>();
-        
+       slots = new ArrayList<DevCard[]>();
+
     }
 
-    //to do
     public void purchaseDevCard(DevCard card, int slot)
     {
+        //if checkSpace is true
+        DevCard[] currSlot = slots.get(slot);
 
-
-
+        //the card with the highest level will be at the end
+        for(int i = 0; i < currSlot.length; i++)
+        {
+            if(currSlot[i] == null)
+            {
+                currSlot[i] = card;
+                break;
+            }
+        }
     }
 
     public boolean checkSpace(DevCard card, int slot)
     {
+        //gets the array of cards on the address "slot"
+        DevCard[] currSlot = slots.get(slot);
+        //current card in the selected slot
+        DevCard currCard;
+
         //checks if the slot is empty or not
-        if(slots.get(slot) == null)
+        if(card.level == 1)
         {
-            if (card.level == 1)
-                return true;
-            else
-                return false;
+           for(int i = 0; i < currSlot.length; i++ )
+           {
+               if(currSlot[i] != null)
+               {
+                   return false;
+               }
+           }
+           return true;
         }
         else
         {
-            //if it isn't, then checks card levels
-            DevCard slotCard = slots.get(slot);
+            //checks slot card level
+            for(int i = 0; i < currSlot.length; i++)
+            {
+                if(currSlot[i] != null)
+                {
+                    currCard = currSlot[i];
+                    if(card.level == currCard.level-1)
+                    {
+                        return true;
+                    }
+                }
 
-            if(card.level == slotCard.level-1)
-                return true;
-            else
-                return false;
+            }
+            return false;
         }
-
 
     }
 
-    public DevCard[3] getFrontCards()
+    public DevCard[] getFrontCards()
     {
+        DevCard[] currSlot;
+        DevCard[] frontCards = new DevCard[3];
+        for(int i = 0; i < slots.size(); i++)
+        {
+            currSlot = slots.get(i);
+            for (int j = 0; j < currSlot.length; j++)
+            {
+                if(currSlot[j+1] == null)
+                {
+                    frontCards[j] = currSlot[j];
+                }
+            }
+        }
+
+        return frontCards;
+
+    }
+
+    public ArrayList<DevCard> getAllCards()
+    {
+        ArrayList<DevCard> allCards = new ArrayList<DevCard>();
+        DevCard[] currSlot;
+        for (int i = 0; i< slots.size(); i++)
+        {
+            currSlot = slots.get(i);
+            for (int j = 0; j < currSlot.length; j++)
+                if(currSlot[j] != null)
+                    allCards.add(currSlot[j]);
+        }
+
+        return allCards;
 
     }
 }
