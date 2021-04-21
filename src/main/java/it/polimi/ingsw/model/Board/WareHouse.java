500a74320228351f73ce66aff848cb8b87a6b34f
pasca99 <antony.pascalino@mail.polimi.it>
2021-04-21 19:10:13 +0200

    Completed JavaDoc for whole Board package

--- src/main/java/it/polimi/ingsw/model/Board/WareHouse.java
+++ src/main/java/it/polimi/ingsw/model/Board/WareHouse.java
@@ -14,8 +14,7 @@ import java.util.Arrays;
  * Together with the {@link StrongBox} is the only place where
  * the Player keeps their {@link Resource}s.
  */
-public class WareHouse
-{
+public class WareHouse {
     private Resource level1[];
     private Resource level2[];
     private Resource level3[];
@@ -30,8 +29,7 @@ public class WareHouse
     /**
      * Instantiates a new WareHouse (with only 3 levels)
      */
-    public WareHouse()
-    {
+    public WareHouse() {
         level1 = new Resource[1];
         level2 = new Resource[2];
         level3 = new Resource[3];
@@ -45,146 +43,120 @@ public class WareHouse
      *
      * @return an ArrayList with the {@link WareHouse}'s levels
      */
-    public ArrayList<Resource[]> getLevels(){
+    public ArrayList<Resource[]> getLevels() {
         return levels;
     }
 
     /**
-     * Add a single {@link Resource} in the chosen level. This action is safe-guaranteed by {@link #checkAvailability(Resource)}
-     * and {@link #checkSpace(int, Resource)} previously called.
+     * Add a single {@link Resource} in the chosen level.
+     * <p>
+     * This action is safe-guaranteed by {@link #checkSpace(int, Resource)} previously called.
      *
      * @param level the level in which it wants add the the {@link Resource}.
      * @param res the {@link Resource} to add.
+     * @return true if
      */
-    public void addResource(int level, Resource res)
-    {
-        Resource[] current;
-        current = levels.get(level);
-        //Add the resource in the first empty space of the array
-        for (int i = 0; i<current.length ; i++)
-        {
-            if(current[i]==null)
-            {
-                current[i]=res;
-                break;
-            }
-        }
-    }
-
-    /**
-     * Gets resources.
-     *
-     * @return the resources
-     */
-//doesn't remove resource, it only gives all the resources
-    public ArrayList<Resource> getResources()
-    {
-        ArrayList<Resource> result=new ArrayList<Resource>();
-        Resource[] currentLevel;
-        for (int i = 0; i<levels.size(); i++)
-        {
-            currentLevel = levels.get(i);
-            for (int j = 0; j < currentLevel.length; j++)
-                if(currentLevel[j] != null)
-                    result.add(currentLevel[j]);
-        }
-
-        return result;
-    }
-
-    /**
-     * Remove resource boolean.
-     *
-     * @param res the res
-     * @return the boolean
-     * @throws ResourceNotAvailable the resource not avaible
-     */
-//remove the resource
-    public Boolean removeResource(Resource res) throws ResourceNotAvailable
-    {
-        //If the resource cannot be removed throw an exception
-        if(!checkAvailability(res))
-            throw new ResourceNotAvailable();
-
-        //Else return true after removing the first occurancy of the resource
-        Resource[] currentLevel;
-        for (int i = 0; i<levels.size(); i++)
-        {
-            currentLevel = levels.get(i);
-            for (int j = 0; j < currentLevel.length; j++)
-                if(currentLevel[j].equals(res))
-                {
-                    currentLevel[j]=null;
+    public boolean addResource(int level, Resource res) {
+        if(checkSpace(level, res)) {
+            Resource[] current;
+            current = levels.get(level);
+            //Add the resource in the first empty space of the array
+            for (int i = 0; i < current.length; i++) {
+                if (current[i] == null) {
+                    current[i] = res;
                     return true;
                 }
-
+            }
         }
-
-        //If somehow there's an error return false
         return false;
     }
 
     /**
-     * Check availability boolean.
+     * Gets all the {@link Resource}s contained in the {@link WareHouse} in every level.
      *
-     * @param res the res
-     * @return the boolean
+     * @return an ArrayList containing all the {@link Resource}s
      */
-    public Boolean checkAvailability(Resource res)
-    {
-        if(this.getResources().contains(res))
-            return true;
-        else
-            return false;
-    }
-
-    /**
-     * Check availability boolean.
-     *
-     * @param resources the resources
-     * @return the boolean
-     */
-    public Boolean checkAvailability(ArrayList<Resource> resources)
-    {
-        //First remove, if there's any, the resource rappresenting the discount
-        resources.removeAll(discount);
-
-        //Check if the warehouse contains the elements in any order.
-        if(this.getResources().containsAll(resources))
-            return true;
-        else
-            return false;
-    }
-
-    /**
-     * Check space boolean.
-     *
-     * @param level the level
-     * @param res   the res
-     * @return the boolean
-     */
-    public Boolean checkSpace(int level, Resource res)
-    {
+    public ArrayList<Resource> getResources() {
+        ArrayList<Resource> result = new ArrayList<Resource>();
         Resource[] currentLevel;
-        for(int i=0; i < levels.size(); i++)
-        {
-            currentLevel=levels.get(i);
+        for (int i = 0; i < levels.size(); i++) {
+            currentLevel = levels.get(i);
+            for (int j = 0; j < currentLevel.length; j++)
+                if (currentLevel[j] != null)
+                    result.add(currentLevel[j]);
+        }
+        return result;
+    }
+
+    /**
+     * Remove a single {@link Resource} from the {@link WareHouse}.
+     *
+     * @param res the {@link Resource} to remove.
+     * @return true if the remove was successful, false if somehow there's an error.
+     * @throws ResourceNotAvailable if the {@link Resource} is not available.
+     */
+    public Boolean removeResource(Resource res) throws ResourceNotAvailable {
+        if (!checkAvailability(res))
+            throw new ResourceNotAvailable();
+
+        Resource[] currentLevel;
+        for (Resource[] level : levels) {
+            currentLevel = level;
+            for (int j = 0; j < currentLevel.length; j++)
+                if (currentLevel[j].equals(res)) {
+                    currentLevel[j] = null;
+                    return true;
+                }
+        }
+        return false;
+    }
+
+    /**
+     * Check if a single {@link Resource} is available in the {@link WareHouse}.
+     *
+     * @param res the {@link Resource} to check.
+     * @return true if the {@link Resource} is available in the {@link WareHouse}, false otherwise.
+     */
+    public boolean checkAvailability(Resource res) {
+        return this.getResources().contains(res);
+    }
+
+    /**
+     * Check if a list of {@link Resource}s is available in the {@link WareHouse}.
+     *
+     * @param resources the ArrayList with all the {@link Resource}s to check.
+     * @return true if all the {@link Resource}s in the ArrayList are available in the {@link WareHouse}, false if even
+     * a single {@link Resource} misses.
+     */
+    public boolean checkAvailability(ArrayList<Resource> resources) {
+        return this.getResources().containsAll(resources);
+    }
+
+
+    /**
+     * Check if the single {@link Resource} can be placed in the {@link WareHouse}'s level according to its rules.
+     *
+     * @param level the level where it wants to add the {@link Resource}.
+     * @param res the {@link Resource} it wants to add in the {@link WareHouse}.
+     * @return true if the {@link Resource} can be added in the wanted level, false otherwise.
+     */
+    public boolean checkSpace(int level, Resource res) {
+        Resource[] currentLevel;
+        for (int i = 0; i < levels.size(); i++) {
+            currentLevel = levels.get(i);
             //If the level doesn't contain any empty space
-            if(i==level && !(Arrays.stream(currentLevel).anyMatch(null)))
+            if (i == level && !(Arrays.stream(currentLevel).anyMatch(null)))
                 return false;
 
-            for( int j=0 ; j< currentLevel.length; j++)
-            {
+            for (int j = 0; j < currentLevel.length; j++) {
                 //If there's the same resource on another level
-                if(currentLevel[j].equals(res) && i != level)
+                if (currentLevel[j].equals(res) && i != level)
                     return false;
                 //If the level contains different resources
-                if(i==level && currentLevel[j]!= null && !currentLevel[j].equals(res))
+                if (i == level && currentLevel[j] != null && !currentLevel[j].equals(res))
                     return false;
             }
         }
         return true;
     }
-
-
 }
