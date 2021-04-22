762fea634680e00b5b20b15007beae83651c29ff
riccardopestrin <10617370@polimi.it>
2021-04-22 18:09:54 +0200

    TCP updated

--- src/main/java/it/polimi/ingsw/connection/LineClient.java
+++ src/main/java/it/polimi/ingsw/connection/LineClient.java
@@ -1,6 +1,10 @@
 package it.polimi.ingsw.connection;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 public class LineClient {
     private String ip;
@@ -11,5 +15,26 @@ public class LineClient {
         this.port = port;
     }
     public void startClient() throws IOException {
+        Socket socket = new Socket(ip, port);
+        System.out.println("Connection established");
+        Scanner socketIn = new Scanner(socket.getInputStream());
+        PrintWriter socketOut = new PrintWriter(socket.getOutputStream());
+        Scanner stdin = new Scanner(System.in);
+        try{
+            while (true){
+                String inputLine = stdin.nextLine();
+                socketOut.println(inputLine);
+                socketOut.flush();
+                String socketLine = socketIn.nextLine();
+                System.out.println(socketLine);
+            }
+        } catch(NoSuchElementException e){
+            System.out.println("Connection closed");
+        } finally {
+            stdin.close();
+            socketIn.close();
+            socketOut.close();
+            socket.close();
+        }
     }
 }
