3cb7ebdcee5d1b1eaacc008a6ffd74bc777508d1
pasca99 <antony.pascalino@mail.polimi.it>
2021-04-23 17:57:53 +0200

    Added some controller classes

---
+++ src/main/java/it/polimi/ingsw/controller/MarketResource.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.controller;
+
+import it.polimi.ingsw.model.Resource;
+
+public class MarketResource {
+    private Resource resource;
+    private int level;
+
+    public MarketResource(Resource resource, int level) {
+        this.resource = resource;
+        this.level = level;
+    }
+
+    public Resource getResource() {
+
+        return resource;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+}
