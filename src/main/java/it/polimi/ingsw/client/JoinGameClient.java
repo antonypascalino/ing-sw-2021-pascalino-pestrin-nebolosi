c28a4abbea01821946b0d335fcffea067b11e14a
lorenzonebolosi <lorenzo.nebolosi@mail.polimi.it>
2021-05-21 15:56:47 +0200

    Made it compile

--- src/main/java/it/polimi/ingsw/client/JoinGameClient.java
+++ src/main/java/it/polimi/ingsw/client/JoinGameClient.java
@@ -8,27 +8,27 @@ import java.io.IOException;
 
 public class JoinGameClient {
 
-//    public static void main(String[] args) throws IOException {
-//        LineClient client = new LineClient("127.0.0.1", 8080);
-//        try {
-//            client.startClient();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        String serverResponse = "test";
-//
-//        Request request = new JoinGameRequest(1,"SickNebo");
-//        System.out.println(request.toString());
-//        try {
-//            Gson gson = new Gson();
-//            String jsonReq = gson.toJson(request);
-//            serverResponse = client.sendMessage(jsonReq);
-//            System.out.println(serverResponse);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        while(true){
-//
-//        }
-//    }
+    public static void main(String[] args) throws IOException {
+        LineClient client = new LineClient("127.0.0.1", 8080);
+        try {
+            client.startClient();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String serverResponse = "test";
+
+        Request request = new JoinGameRequest(1,"SickNebo");
+        System.out.println(request.toString());
+        try {
+            Gson gson = new Gson();
+            String jsonReq = gson.toJson(request);
+            serverResponse = client.sendMessage(jsonReq);
+            System.out.println(serverResponse);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        while(true){
+
+        }
+    }
 }
