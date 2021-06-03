7e38b041b3fb0654555b190121e9c0a429f0f045
lorenzonebolosi <lorenzo.nebolosi@mail.polimi.it>
2021-06-03 13:38:39 +0200

    Tested the Discount and extradep card

--- src/test/java/LeaderCardTest.java
+++ src/test/java/LeaderCardTest.java
@@ -9,6 +9,8 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
+import static org.junit.Assert.assertEquals;
+
 public class LeaderCardTest {
     /**
      * Tries to wrap a BasicPlayer in a new type of player after using a leadercard
@@ -109,6 +111,7 @@ public class LeaderCardTest {
         tmp.getBoard().getSlot().placeCard(DefaultCreator.produceDevCard().get(42),0);
         System.out.println(card.getClassName() + " " +card.getID());
         card.playCard();
+        assertEquals(game.getPlayers().get(0).getClass().getName(),"it.polimi.ingsw.model.Player.ExtraProdPlayer");
         System.out.println(game.getPlayers().get(0).getClass().getName());
     }
 
@@ -210,6 +213,214 @@ public class LeaderCardTest {
         tmp.getBoard().getSlot().placeCard(DefaultCreator.produceDevCard().get(14),2);
         System.out.println(card.getClassName() + " " +card.getID());
         card.playCard();
+        assertEquals(game.getPlayers().get(0).getClass().getName(),"it.polimi.ingsw.model.Player.ChangeResPlayer");
+        System.out.println(game.getPlayers().get(0).getClass().getName());
+    }
+
+    @Test
+    public void TestDiscount()
+    {
+        //First create a new game and add a player
+        ArrayList<Game> games = new ArrayList<>();
+        Request request = new NewGameRequest("SickNebo", 3);
+        //This code has been copied from the client handler class
+        if(request instanceof NewGameRequest)
+        {
+            if(request instanceof NewGameRequest)
+            {
+                //If there's no game on the server create the first one
+                Game lastGame = null;
+                if (games.size() != 0)
+                    lastGame = games.get(games.size()-1);
+                //If there's no game or the last one has reached the maximum player, it doesn't check it if games.size==0
+                //Create a new game
+                if (games.size() == 0 || !(lastGame.getPlayers().size() < lastGame.getMax()))
+                {
+                    int gameId;
+                    if(games.size() != 0)
+                        gameId = games.get(games.size()-1).getGameId() +1;
+                    else
+                        gameId=0;
+                    ArrayList<Player> tmp = new ArrayList<Player>();
+
+                    Player newPlayer = new BasicPlayer(((NewGameRequest) request).getNickname());
+                    tmp.add(newPlayer);
+                    Game newGame = new Game(tmp,DefaultCreator.produceDevCard(),gameId,((NewGameRequest) request).getPlayers());
+                    newPlayer.setGame(newGame);
+                    games.add(newGame);
+                    System.out.println("Player "+((NewGameRequest) request).getNickname()+ " added to the new game "+newGame.getGameId());
+                }
+                //If it hasn't alrady reached the maximum numner of players
+                //add the new player
+                else
+                {
+                    Player newPlayer = new BasicPlayer(((NewGameRequest) request).getNickname());
+                    lastGame.addPlayer(newPlayer);
+                    newPlayer.setGame(lastGame);
+                    System.out.println("Player "+((NewGameRequest) request).getNickname()+ " added to game "+lastGame.getGameId());
+                }
+            }
+        }
+
+
+        //Now send a new request for buying
+        Player tmp = games.get(0).getPlayers().get(0);
+        //Get the second row of the market
+        ArrayList<Resource> resources = games.get(0).getTable().market.seeRow(2);
+        System.out.println(games.get(0).getTable().market.toString());
+        ArrayList<MarketResource> mappedRes = new ArrayList<MarketResource>();
+        for (int i=0; i<3;i++)
+        {
+            Resource res= resources.get(i);
+            System.out.println(res);
+            MarketResource resource = new MarketResource(res, i);
+            mappedRes.add(resource);
+        }
+
+
+
+        Request test = new MarketRequest(MarketDimension.ROW, 2, games.get(0).getGameId(), tmp.getNickName(), mappedRes);
+        games.get(0).notify(test);
+        System.out.println(tmp.getAllResources());
+        System.out.println(games.get(0).getTable().market.toString());
+
+
+
+        //Now send a new request for buying
+        tmp = games.get(0).getPlayers().get(0);
+        //Get the second row of the market
+        resources = games.get(0).getTable().market.seeRow(2);
+        System.out.println(games.get(0).getTable().market.toString());
+        mappedRes = new ArrayList<MarketResource>();
+        for (int i=0; i<3;i++)
+        {
+            Resource res= resources.get(i);
+            System.out.println(res);
+            MarketResource resource = new MarketResource(res, i);
+            mappedRes.add(resource);
+        }
+
+
+
+        test = new MarketRequest(MarketDimension.ROW, 2, games.get(0).getGameId(), tmp.getNickName(), mappedRes);
+        games.get(0).notify(test);
+        System.out.println(tmp.getAllResources());
+        System.out.println(games.get(0).getTable().market.toString());
+        Game game = games.get(0);
+        //it gets the first card which is a n extra prod card that requires
+        LeaderCard card = DefaultCreator.produceLeaderCard().get(9);
+        card.assignTo(tmp);
+        tmp.getBoard().getSlot().placeCard(DefaultCreator.produceDevCard().get(13),1);
+        tmp.getBoard().getSlot().placeCard(DefaultCreator.produceDevCard().get(27),2);
+        System.out.println(card.getClassName() + " " +card.getID());
+        card.playCard();
+        assertEquals(game.getPlayers().get(0).getClass().getName(),"it.polimi.ingsw.model.Player.DiscountedPlayer");
+        System.out.println(game.getPlayers().get(0).getClass().getName());
+    }
+
+    @Test
+    public void TestExtraDep()
+    {
+        //First create a new game and add a player
+        ArrayList<Game> games = new ArrayList<>();
+        Request request = new NewGameRequest("SickNebo", 3);
+        //This code has been copied from the client handler class
+        if(request instanceof NewGameRequest)
+        {
+            if(request instanceof NewGameRequest)
+            {
+                //If there's no game on the server create the first one
+                Game lastGame = null;
+                if (games.size() != 0)
+                    lastGame = games.get(games.size()-1);
+                //If there's no game or the last one has reached the maximum player, it doesn't check it if games.size==0
+                //Create a new game
+                if (games.size() == 0 || !(lastGame.getPlayers().size() < lastGame.getMax()))
+                {
+                    int gameId;
+                    if(games.size() != 0)
+                        gameId = games.get(games.size()-1).getGameId() +1;
+                    else
+                        gameId=0;
+                    ArrayList<Player> tmp = new ArrayList<Player>();
+
+                    Player newPlayer = new BasicPlayer(((NewGameRequest) request).getNickname());
+                    tmp.add(newPlayer);
+                    Game newGame = new Game(tmp,DefaultCreator.produceDevCard(),gameId,((NewGameRequest) request).getPlayers());
+                    newPlayer.setGame(newGame);
+                    games.add(newGame);
+                    System.out.println("Player "+((NewGameRequest) request).getNickname()+ " added to the new game "+newGame.getGameId());
+                }
+                //If it hasn't alrady reached the maximum numner of players
+                //add the new player
+                else
+                {
+                    Player newPlayer = new BasicPlayer(((NewGameRequest) request).getNickname());
+                    lastGame.addPlayer(newPlayer);
+                    newPlayer.setGame(lastGame);
+                    System.out.println("Player "+((NewGameRequest) request).getNickname()+ " added to game "+lastGame.getGameId());
+                }
+            }
+        }
+
+
+        //Now send a new request for buying
+        Player tmp = games.get(0).getPlayers().get(0);
+        //Get the second row of the market
+        ArrayList<Resource> resources = games.get(0).getTable().market.seeRow(2);
+        System.out.println(games.get(0).getTable().market.toString());
+        ArrayList<MarketResource> mappedRes = new ArrayList<MarketResource>();
+        for (int i=0; i<3;i++)
+        {
+            Resource res= resources.get(i);
+            System.out.println(res);
+            MarketResource resource = new MarketResource(res, i);
+            mappedRes.add(resource);
+        }
+
+
+
+        Request test = new MarketRequest(MarketDimension.ROW, 2, games.get(0).getGameId(), tmp.getNickName(), mappedRes);
+        games.get(0).notify(test);
+        System.out.println(tmp.getAllResources());
+        System.out.println(games.get(0).getTable().market.toString());
+
+
+
+        //Now send a new request for buying
+        tmp = games.get(0).getPlayers().get(0);
+        //Get the second row of the market
+        resources = games.get(0).getTable().market.seeRow(2);
+        System.out.println(games.get(0).getTable().market.toString());
+        mappedRes = new ArrayList<MarketResource>();
+        for (int i=0; i<3;i++)
+        {
+            Resource res= resources.get(i);
+            System.out.println(res);
+            MarketResource resource = new MarketResource(res, i);
+            mappedRes.add(resource);
+        }
+
+
+
+        test = new MarketRequest(MarketDimension.ROW, 2, games.get(0).getGameId(), tmp.getNickName(), mappedRes);
+        games.get(0).notify(test);
+        System.out.println(tmp.getAllResources());
+        System.out.println(games.get(0).getTable().market.toString());
+        Game game = games.get(0);
+        //it gets the first card which is a n extra prod card that requires
+        LeaderCard card = DefaultCreator.produceLeaderCard().get(13);
+        card.assignTo(tmp);
+        tmp.getBoard().getStrongBox().addResource(Resource.SHIELD);
+        tmp.getBoard().getStrongBox().addResource(Resource.SHIELD);
+        tmp.getBoard().getStrongBox().addResource(Resource.SHIELD);
+        tmp.getBoard().getStrongBox().addResource(Resource.SHIELD);
+        tmp.getBoard().getStrongBox().addResource(Resource.SHIELD);
+        tmp.getBoard().getStrongBox().addResource(Resource.SHIELD);
+
+        System.out.println(card.getClassName() + " " +card.getID());
+        card.playCard();
+        assertEquals(game.getPlayers().get(0).getClass().getName(),"it.polimi.ingsw.model.Player.ExtraDepositPlayer");
         System.out.println(game.getPlayers().get(0).getClass().getName());
     }
 }
